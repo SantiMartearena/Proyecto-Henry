37f532ec7ff14de5c8bbc952e015301e03e74646
SantiMartearena <124310420+SantiMartearena@users.noreply.github.com>
2023-06-12 15:36:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -108,5 +108,4 @@ def get_director(director: str):
     resp = df1[["title", "release_year", "return", "budget", "ganancia"]]
 
     return [director, retorno_director, resp]
-df1.loc[:, "ganancia"] = df1["revenue"] - df1["budget"]
 
